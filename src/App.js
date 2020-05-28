fc8c74e9ab76520bc602fb8f6debdc75a2bb74d0
yaohuangguan <yaob@miamioh.edu>
2020-05-28 22:32:13 +0800

    added white background move reverse

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import logo from './assets/btn.png';
+import logo from './assets/button.png';
 import './App.css';
 
 function App() {
   const handleClick = (e) => {
     const target = e.target
     const container = document.querySelector('.container')
+    const template = document.querySelector('.white-template')
 
-    console.log(target)
     target.classList.add('click-rotate')
     container.classList.add('click-move')
+    template.classList.add('click-move-reverse')
   }
   return (
     <div className="App">
       <header className="App-header">
-        <div className='container'>
+      <div className='white-template'></div>
+
+      <div className='container'>
           <img src={logo} className="App-logo" alt="logo" onClick={handleClick} />
         </div>
        
