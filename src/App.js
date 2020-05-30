80e300d93a3e9e07404dcfe3c8a8e82b4ec14373
yaohuangguan <yaob@miamioh.edu>
2020-05-30 22:27:21 +0800

    regular push

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React from "react";
 import logo from "./assets/button.png";
 import "./App.css";
 import WelcomeButton from "./components/WelcomeButton";
-function App() {
-  const handleClick = (e) => {
+import WhiteBackground from "./components/Background";
+const animationName = "animate__fadeInDown";
+const App = () => {
+  const handleClick = React.useCallback((e) => {
     const target = e.target;
     const container = document.querySelector(".container");
     const template = document.querySelector(".white-template");
-
+    const bg = document.querySelector(".white-background");
     target.classList.add("click-rotate");
     container.classList.add("click-move");
     template.classList.add("click-move-reverse");
-  };
+    setTimeout(() => {
+      bg.style.display = "block";
+      bg.classList.add("animate__animated", animationName);
+    }, 3500);
+  }, []);
   return (
-    <div className="App">
+    <React.Fragment>
       <div className="App-header">
         <WelcomeButton logo={logo} handleClick={handleClick} />
-    
+        <WhiteBackground />
       </div>
-    </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/components/WelcomeButton.js
+++ src/components/WelcomeButton.js
@@ -1,15 +1,28 @@
 import React from "react";
+const animateName = "animate__tada";
+const WelcomeButton = React.memo(({ logo, handleClick }) => {
+  const onMouseEnter = () => {
+    const bg = document.querySelector(".container");
 
-const WelcomeButton = ({ logo, handleClick }) => {
+    bg.classList.add("animate__animated", animateName);
+  };
+  const onMouseLeave = () => {
+    const bg = document.querySelector(".container");
+    bg.classList.remove(animateName);
+  };
   return (
     <>
       <div className="white-template"></div>
 
-      <div className="container">
+      <div
+        className="container"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
         <img src={logo} className="App-logo" alt="logo" onClick={handleClick} />
       </div>
     </>
   );
-};
+});
 
 export default WelcomeButton;
